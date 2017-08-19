e2726e0ffa1ff2265a2344b9ffe751a4fa130cf7
anish12341 <anishpatel345@gmail.com>
2017-08-19 19:45:02 +0530

    Update ajaxrequest.php

--- ajaxrequest.php
+++ ajaxrequest.php
@@ -1,8 +1,7 @@
 <?php
-
-$q=$_REQUEST["q"];
+//this is called from tweets.php as ajax request for dynamic content changing in slideshow , based on screen name which is clicked on
+$q=$_REQUEST["q"]; //storing screen name of follower which is clicked on in $q 
 require 'index.php';
-//$r=$_SESSION["callback"];
-  $jsobj=json_encode($response);
+ $jsobj=json_encode($response);
  echo $jsobj;
 ?>
