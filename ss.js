409681fb7ac374b6e6968e3b3678f8c341ffa078
anish12341 <anishpatel345@gmail.com>
2017-08-19 20:15:15 +0530

    Update ss.js

--- ss.js
+++ ss.js
@@ -1,15 +1,17 @@
-$(function() {
-
-	var width= $("#slider").width();
+$(function() {                     //FOR SLIDESHOW FUNCTIONING USING JQUERY
+				   //LOGIC:Inside container ul is created of width 10*width(container) and inside ul li(s) are created 
+	                           //       with width(container) at each interval ul will change its margin-left property in order 
+	                           //       to show next li .
+    var width= $("#slider").width();        //#slider is the container inside which ul is stored
     var animationSpeed = 1000;
     var pause = 4000;
     var currentSlide = 1;
     
     //cache DOM elements
-    var $slider = $('#slider');//div
-    var $slideContainer = $('.slides', $slider);//ul
+    var $slider = $('#slider');                          //div(container)
+    var $slideContainer = $('.slides', $slider);         //ul
 	$slideContainer.width(10*width);
-    var $slides = $('.slide', $slider);//li
+    var $slides = $('.slide', $slider);                  //li
 	$slides.width(width);
       alert($slides.width(width)+"in starting");
 	
